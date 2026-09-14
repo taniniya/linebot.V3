34152fb50a4dbcf763c858b4b786f4636cc37201
taniniya <xiaofengbaohao@gmail.com>
2026-09-14 21:44:51 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1102,8 +1102,7 @@ function helpText() {
     "/admins",
     "/resetrank",
     "/offbot @メンション",
-    "/onbot @メンション",
-    "/adminplususer <LINE USER ID>"
+    "/onbot @メンション"
   ].join("\n");
 }
 
